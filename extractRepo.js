ea9271da0a5d229da7a6066f3139d8c2f16010ad
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2023-11-22 21:52:46 +0300

    Added UUID-based temporary directory for repository cloning

--- extractRepo.js
+++ extractRepo.js
@@ -1,29 +1,33 @@
-const fs = require('fs');
-const path = require('path');
-const { execSync } = require('child_process');
-const os = require('os'); 
+const fs = require("fs");
+const path = require("path");
+const { execSync } = require("child_process");
+const { v4: uuidv4 } = require("uuid");
+const os = require("os");
 
-execSync(`git clone https://github.com/NucleoidJS/Nucleoid`);
+const tempDir = os.tmpdir();
+const folderId = uuidv4();
+
+const repoDir = path.join(tempDir, folderId);
+
+execSync(`git clone https://github.com/NucleoidJS/Nucleoid ${repoDir}`);
 
 function readDirR(dir) {
-    return fs.statSync(dir).isDirectory()
-        ? Array.prototype.concat(...fs.readdirSync(dir).map(f => readDirR(path.join(dir, f))))
-        : dir;
+  return fs.statSync(dir).isDirectory()
+    ? Array.prototype.concat(
+        ...fs.readdirSync(dir).map((f) => readDirR(path.join(dir, f)))
+      )
+    : dir;
 }
 
-let files = readDirR('Nucleoid');
+let files = readDirR(repoDir);
 
-let backupContent = '';
+let backupContent = "";
 
-files.forEach(file => {
-    let content = fs.readFileSync(file, 'utf8');
-    backupContent += `# ${file}\n${content}\n`;
+files.forEach((file) => {
+  let content = fs.readFileSync(file, "utf8");
+  backupContent += `# ${file}\n\n${content}\n`;
 });
 
 console.log(backupContent);
 
-if (os.platform() === 'win32') {
-    execSync(`rmdir /s /q Nucleoid`);
-} else {
-    execSync(`rm -Rf Nucleoid`);
-}
+fs.rmdirSync(repoDir, { recursive: true });
