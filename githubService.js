83b8920e3a26ce3eb8cc15b3cd0c51a5ba6d6749
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2023-11-22 19:31:42 +0300

    Add getRepos function to githubService for fetch repos dynamicly

--- githubService.js
+++ githubService.js
@@ -2,6 +2,10 @@ const { get } = require("axios").default;
 
 class GithubService {
   
+    async getRepos() {
+    const { data } = await get(`https://api.github.com/orgs/NucleoidJS/repos`);
+    return data.map((repo) => repo.name);
+  }
 
   async getContributors(repo) {
     const { data } = await get(
