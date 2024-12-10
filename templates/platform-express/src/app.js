c43bf73263f18dc64773f562603634fa202c065b
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-12-10 23:24:56 +0300

    Remove authorization middleware from app level

--- templates/platform-express/src/app.js
+++ templates/platform-express/src/app.js
@@ -1,12 +1,8 @@
 const platform = require("@nucleoidai/platform-express");
-const { authorize } = require("@nucleoidai/platform-express/authorization");
 
-const { authorization, express } = platform;
+const { express } = platform;
 
 const app = express();
-
-app.use(authorization.verify);
-app.use(authorize("ADMIN"));
 
 // Add your routes here
 
