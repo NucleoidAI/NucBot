a222d3a50499f937f6e39f20bbe8ac44dd589cf4
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-12-13 02:59:18 +0300

    Change createProject according changes

--- utils/createProject.js
+++ utils/createProject.js
@@ -3,7 +3,10 @@ const path = require("path");
 const uuid = require("uuid");
 const chalk = require("chalk");
 
-const copyDirectory = (src, dest, projectName, appId) => {
+const createConfig = require("./createConfig");
+const formatObjectToString = require("./formatObjectToString");
+
+const copyDirectory = (src, dest, projectName, appId, login, type) => {
   if (!fs.existsSync(dest)) {
     fs.mkdirSync(dest);
   }
@@ -15,32 +18,62 @@ const copyDirectory = (src, dest, projectName, appId) => {
     const destPath = path.join(dest, entry.name);
 
     if (entry.isDirectory()) {
-      copyDirectory(srcPath, destPath, projectName, appId);
+      copyDirectory(srcPath, destPath, projectName, appId, login, type);
     } else {
       let content = fs.readFileSync(srcPath, "utf8");
 
+      const config = createConfig(type, entry.name);
+
       content = content
+        .
+        replace(
+          /\{\{project\}\}/g,
+          login.enabled
+            ? `,project: ${formatObjectToString(config.project, 2)}`
+            : ""
+        )
+        .replace(
+          /\{\{login\}\}/g,
+          login.enabled
+            ? `,login: ${formatObjectToString(config.login, 2)},`
+            : ""
+        )
+        .replace(
+          /\{\{projectBar\}\}/g,
+          login.enabled
+            ? `projectBar: ${formatObjectToString(config.projectBar, 2)}`
+            : ""
+        )
         .replace(/\{\{projectName\}\}/g, projectName)
-        .replace(/\{\{appId\}\}/g, appId);
+        .replace(/\{\{appId\}\}/g, appId)
+        .replace(
+          /\{\{layout1\}\}/g,
+          login.enabled ? `DashboardLayout` : "MainLayout"
+        )
+        .replace(
+          /\{\{layout2\}\}/g,
+          login.enabled ? `FullScreenLayout` : "SimpleLayout"
+        );
 
       fs.writeFileSync(destPath, content);
     }
   }
 };
 
-const createProject = (projectName, template) => {
+const createProject = (projectConfig) => {
   try {
+    console.log(projectConfig)
+    const { name, type, login } = projectConfig;
     const appId = uuid.v4();
 
-    const templatePath = path.join(__dirname, "..", "templates", template);
-
-    const projectPath = path.join(process.cwd(), projectName);
+    const templatePath = path.join(__dirname, "..", "templates", type);
+    const projectPath = path.join(process.cwd(), name);
 
     if (fs.existsSync(projectPath)) {
-      throw new Error(`Directory ${projectName} already exists`);
+      throw new Error(`Directory ${name} already exists`);
     }
 
-    copyDirectory(templatePath, projectPath, projectName, appId);
+    copyDirectory(templatePath, projectPath, name, appId, login, type);
 
     return true;
   } catch (error) {
@@ -50,3 +83,4 @@ const createProject = (projectName, template) => {
 };
 
 module.exports = createProject;
+
